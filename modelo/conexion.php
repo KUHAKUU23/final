81e3c7e1ec59694a5252976e99f89020cde3cf5d
l181000055@mazatlan.tecnm.mx <l181000055@mazatlan.tecnm.mx>
2022-05-27 10:55:22 -0600

    listo

---
+++ modelo/conexion.php
@@ -0,0 +1,9 @@
+<?php
+    class Conexion{
+        private $conexion;
+        public function _construct(){
+             $this->conection=new mtsqli('localhost','root','','practica1');
+}
+
+}
+?>
